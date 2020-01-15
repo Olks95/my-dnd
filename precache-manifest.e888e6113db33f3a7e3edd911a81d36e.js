ce1777725343b2576d5e23ba5462cb4ace1046de
Olks jobb <26792432+Olks95@users.noreply.github.com>
2020-01-15 09:30:34 +0100

    Updates

---
+++ precache-manifest.e888e6113db33f3a7e3edd911a81d36e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97981001169124cf8d1da6f806ef82e3",
+    "url": "/my-dnd/index.html"
+  },
+  {
+    "revision": "7277fbede7bc8fa9bdae",
+    "url": "/my-dnd/static/css/main.86c00371.chunk.css"
+  },
+  {
+    "revision": "991cf6b9699899d73985",
+    "url": "/my-dnd/static/js/2.56a8d993.chunk.js"
+  },
+  {
+    "revision": "0c08a48b164499df78c0607b14eb2c76",
+    "url": "/my-dnd/static/js/2.56a8d993.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7277fbede7bc8fa9bdae",
+    "url": "/my-dnd/static/js/main.fdf8aa2d.chunk.js"
+  },
+  {
+    "revision": "d59d3fdafda96f9d0c7d",
+    "url": "/my-dnd/static/js/runtime-main.96b9ab7a.js"
+  }
+]);
